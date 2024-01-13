a3a09f90d5ed6bcb3d8d9b1133775b17f5e7e37c
JULIUS NYERERE <nyerereoffice@gmail.com>
2024-01-13 20:42:19 +0300

    Add files via upload

---
+++ Thirteen/corey-schafer-blog/blog/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render
+
+from .models import Post
+
+from django.views.generic import ListView
+
+
+# def home(request):
+#     posts = Post.objects.all()
+#     context = {'posts': posts}
+#     return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html' # <app> / <model>_<viewtype>.html
+    context_object_name = 'posts'
+
+
+def about(request):
+    posts = Post.objects.all()
+    context = {'posts': posts}
+    return render(request, 'blog/about.html', context)
+
+    
