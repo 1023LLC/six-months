e571965aeafa7b0a1f9519ac68e3a26989337650
JULIUS NYERERE <nyerereoffice@gmail.com>
2024-02-05 23:57:40 +0300

    Update views.py
    *Adding a homepage*

--- Fifteen/blog/genres/views.py
+++ Fifteen/blog/genres/views.py
@@ -2,5 +2,10 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
+from django.views import View
+
 def index(request):
     return HttpResponse('<h1>Hello, 1023Xc!</h1>')
+
+class home(View):
+    return HttpResponse('<h1>This is the HomePage</h1>')
