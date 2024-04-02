5a26b8da4d32747f4fd26391fcd41fa4b228c10d
julius <nyerereoffice@gmail.com>
2024-04-02 15:52:20 +0300

    Worked On About and Work Sections

---
+++ portfolio-1.0/src/App.js
@@ -0,0 +1,19 @@
+import About from "./components/About";
+import Hero from "./components/Hero";
+import Navbar from "./components/Navbar";
+import Skills from "./components/Skills";
+import Work from "./components/Work";
+
+function App() {
+  return (
+    <div className="App">
+        <Navbar />
+        <Hero />
+        <Skills />
+        <About />
+        <Work />
+    </div>
+  );
+}
+
+export default App;
